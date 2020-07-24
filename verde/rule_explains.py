e3810a1791ff58b567b9c5bac1d21ba07c2a9dc1
trubens <toby.rubenstein@letterboxes.org>
2020-07-24 16:56:25 +0100

    Graph produced and can be imported into Gephi

--- verde/rule_explains.py
+++ verde/rule_explains.py
@@ -2,7 +2,6 @@ import verde.verde_utils as vu
 import logging
 import json
 import networkx as nx
-import matplotlib.pyplot as plt
 
 
 def get_schema_nodes_and_edges(domain_schema_file_path):
@@ -107,6 +106,7 @@ def get_schema_nodes_and_edges(domain_schema_file_path):
             path = ['properties']
         if dom_path is None:  # domain specific terms only, no json schema reserved words
             dom_path = []
+        nonlocal schema  # we will need to expand $refs to definitions hence access to entire schema
 
         for k, v in d.items():
             path.append(k)
@@ -115,22 +115,22 @@ def get_schema_nodes_and_edges(domain_schema_file_path):
             if path[-2] == 'properties':  # all domain specific nodes will be properties
                 dom_path.append(k)
                 if isinstance(v, dict) and '$ref' in v.keys() and v['$ref'].split('/')[1] == 'properties':
-                    pass  # we've looked ahead and seen this is a jump
+                    pass  # We've looked ahead and seen this is a jump so don't want to create the edge or node yet.
+                    # Yes it is ugly, but for definitions we want to expand fully which happens in next recursion.
                 else:
                     logging.debug(f"ADDING NODE {'.'.join(dom_path)}")
                     nodes.append(('.'.join(dom_path), {"short": k}))
-                    if len(prev_dom_path) > 0:
+                    if len(prev_dom_path) > 0:  # only create edge if we have a parent
                         edge = ('.'.join(prev_dom_path), '.'.join(dom_path))
                         logging.debug('PROPERTY EDGE FROM {} TO {}'.format(edge[0], edge[1]))
                         property_edges.append(edge)
 
-            if k == '$ref' and path[-3] == 'properties':  # normal schema reference
+            if k == '$ref' and path[-3] == 'properties':  # normal schema cross-reference
                 if v.split('/')[1] == 'definitions':  # expand the ref to definitions
-                    nonlocal schema
                     schema_part = schema
                     for node in v.split('/')[1:]:  # ignore leading '#' and walk down schema to the node referenced
                         schema_part = schema_part[node]
-                    v = schema_part
+                    v = schema_part  # expansion has occurred now, so the definition will get walked
                 elif v.split('/')[1] == 'properties':  # capture the jump to another property and walk no further
                     ref_path = '.'.join(v.split('/')[2:][::2])
                     edge = ('.'.join(prev_dom_path[0:-1]), ref_path)
@@ -157,12 +157,6 @@ def get_schema_nodes_and_edges(domain_schema_file_path):
                     else:
                         logging.warning('ignored verde_rule_directive {} with values {}'.format(vrd_k, vrd_v))
 
-
-
-            # logging.debug(f"PATH {'.'.join(path)}")
-            # logging.debug(f"\t PREV_DOM_PATH {'.'.join(prev_dom_path)}")
-            # logging.debug(f"\t CURR_DOM_PATH {'.'.join(dom_path)}")
-
             # Recurse over remainder of schema
             if isinstance(v, dict):
                 walk(v, path, dom_path)
@@ -171,8 +165,8 @@ def get_schema_nodes_and_edges(domain_schema_file_path):
                     if isinstance(v_item, dict):
                         walk(v_item, path, dom_path)
 
-            # symmetry with appends above to walk back up by one node before next loop
-            if path[-3:-1] == ['verde_rule_directive', 'explains']:
+            # symmetry with the appends above to walk back up by one node before next loop
+            if k == 'verde_rule_directive' and path[-1] == 'explains':
                 path.pop()
             if path[-2] == 'properties':
                 dom_path.pop()
@@ -190,17 +184,24 @@ def get_schema_nodes_and_edges(domain_schema_file_path):
     return nodes, property_edges, jump_edges, explain_edges
 
 
-def build_graph(nodes, std_edges, explain_edges, std_edge_weight=1, explain_edge_weight=0.1, export_file=None):
+def build_graph(nodes, property_edges, jump_edges, explain_edges,
+                property_edge_weight=1, jump_edge_weight=0.5, explain_edge_weight=0.1, export_file=None):
     """
     Takes nodes and edges to build a directed, weighted graph. Standard edges have more weight than explain edges
     :param nodes: a list of tuples of long node name and dict with short names. e.g. long=funder.budget, short=budget
-    :param std_edges: list of tuples of long form nodes, for standard schema relationships
-    :param explain_edges: per std_edges but for explains cross-references between nodes.
+    :param property_edges: list of tuples of long form nodes, for standard schema relationships
+    :param jump_edges: per references to another property
+    :param explain_edges: per property_edges but for explains cross-references between nodes.
+    :param explain_edge_weight:
+    :param jump_edge_weight:
+    :param property_edge_weight:
+    :param export_file:
     :return: a networkx graph
     """
     g = nx.DiGraph()
     g.add_nodes_from(nodes)
-    g.add_edges_from(std_edges, weight=std_edge_weight)
+    g.add_edges_from(property_edges, weight=property_edge_weight)
+    g.add_edges_from(jump_edges, weight=jump_edge_weight)
     g.add_edges_from(explain_edges, weight=explain_edge_weight)
 
     if export_file:
@@ -216,5 +217,5 @@ def create_asp_for_explains(domain_schema_file_path, input_mapping_file_path, sc
 if __name__ == "__main__":
     vu.configure_logger('rule_explains.log', level=logging.DEBUG)
     nodes_edges = get_schema_nodes_and_edges('../schemas/verde_asc_domain_schema.json')
-#    graph = build_graph(*nodes_edges, export_file='schema_v2')
+    graph = build_graph(*nodes_edges, export_file='schema_3edgetypes')
     pass
