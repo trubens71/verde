f8058bece13b4ca36d72e5dc941742f8708320d8
trubens <toby.rubenstein@letterboxes.org>
2020-10-28 11:23:23 +0000

    Regression run in master and logs saved to laboratory. General tidy up.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='verde',
-    scripts=['scripts/verde.py','scripts/create_vis.py', 'scripts/inspect_multi_models.py', 'zsh/lp_clingo_vl.zsh'],
+    scripts=['scripts/verde.py'],
     packages=['src'],
     version='0.2',
     url='https://github.com/trubens71/verde',
